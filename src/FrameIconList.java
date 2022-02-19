fa80aa90b7774db07410489f35b7e371cb3e98fb
aylivex <94047625+aylivex@users.noreply.github.com>
2022-02-19 15:36:33 +0000

    FrameIconList: Re-arranged code Used method reference instead of anonymous class

--- src/FrameIconList.java
+++ src/FrameIconList.java
@@ -15,6 +15,19 @@ import javax.swing.border.EmptyBorder;
 
 public class FrameIconList {
 
+    private static final Color[] colors = new Color[]{
+            Color.GREEN,
+            Color.RED,
+            Color.YELLOW,
+            Color.WHITE,
+            Color.CYAN,
+            Color.MAGENTA,
+            Color.PINK,
+            Color.ORANGE
+    };
+
+    private static final int[] sizes = new int[64];
+
     public static BufferedImage getImage(int size, Color color) {
         BufferedImage i = new BufferedImage(
                 size, size, BufferedImage.TYPE_INT_RGB);
@@ -28,7 +41,7 @@ public class FrameIconList {
         if (off > 1) {
             g.drawRect(0, 0, size - 1, size - 1);
         }
-        g.drawString("" + size, off, size-off);
+        g.drawString("" + size, off, size - off);
 
         g.dispose();
 
@@ -36,66 +49,49 @@ public class FrameIconList {
     }
 
     public static void main(String[] args) {
-        final Color[] colors = {
-                Color.GREEN,
-                Color.RED,
-                Color.YELLOW,
-                Color.WHITE,
-                Color.CYAN,
-                Color.MAGENTA,
-                Color.PINK,
-                Color.ORANGE
-        };
-
-        int s = 64;
-        final int[] sizes = new int[s];
-
         for (int ii = 0; ii < sizes.length; ii++) {
             sizes[ii] = 16 + (ii * 2);
         }
 
-        Runnable r = new Runnable() {
-
-            @Override
-            public void run() {
-                // the GUI as seen by the user (without frame)
-                JPanel gui = new JPanel(new BorderLayout());
-                gui.setBorder(new EmptyBorder(2, 3, 2, 3));
-                gui.setBackground(Color.WHITE);
-
-                ArrayList<BufferedImage> images = new ArrayList<>();
-                Vector<ImageIcon> icons = new Vector<>();
-                for (int ii = 0; ii < sizes.length; ii++) {
-                    BufferedImage bi = getImage(
-                            sizes[ii],
-                            colors[ii % colors.length]);
-                    images.add(bi);
-                    ImageIcon imi = new ImageIcon(bi);
-                    icons.add(imi);
-                }
-                JList<ImageIcon> list = new JList<>(icons);
-                list.setVisibleRowCount(6);
-                gui.add(new JScrollPane(list));
-
-                JFrame f = new JFrame("Icon size usage");
-                f.setIconImages(images);
-                f.add(gui);
-                // Ensures JVM closes after frame(s) closed and
-                // all non-daemon threads are finished
-                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                // See http://stackoverflow.com/a/7143398/418556 for demo.
-                f.setLocationByPlatform(true);
-
-                // ensures the frame is the minimum size it needs to be
-                // in order display the components within it
-                f.pack();
-                // should be done last, to avoid flickering, moving,
-                // resizing artifacts.
-                f.setVisible(true);
-            }
-        };
         // Swing GUIs should be created and updated on the EDT
         // http://docs.oracle.com/javase/tutorial/uiswing/concurrency/initial.html
-        SwingUtilities.invokeLater(r);
+        SwingUtilities.invokeLater(FrameIconList::createUI);
+    }
+
+    private static void createUI() {
+        // the GUI as seen by the user (without frame)
+        JPanel gui = new JPanel(new BorderLayout());
+        gui.setBorder(new EmptyBorder(2, 3, 2, 3));
+        gui.setBackground(Color.WHITE);
+
+        ArrayList<BufferedImage> images = new ArrayList<>(sizes.length);
+        Vector<ImageIcon> icons = new Vector<>(sizes.length);
+        for (int ii = 0; ii < sizes.length; ii++) {
+            BufferedImage bi = getImage(
+                    sizes[ii],
+                    colors[ii % colors.length]);
+            images.add(bi);
+            ImageIcon imi = new ImageIcon(bi);
+            icons.add(imi);
+        }
+        JList<ImageIcon> list = new JList<>(icons);
+        list.setVisibleRowCount(6);
+        gui.add(new JScrollPane(list));
+
+        JFrame f = new JFrame("Icon size usage");
+        f.setIconImages(images);
+        f.add(gui);
+        // Ensures JVM closes after frame(s) closed and
+        // all non-daemon threads are finished
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        // See http://stackoverflow.com/a/7143398/418556 for demo.
+        f.setLocationByPlatform(true);
+
+        // ensures the frame is the minimum size it needs to be
+        // in order display the components within it
+        f.pack();
+        // should be done last, to avoid flickering, moving,
+        // resizing artifacts.
+        f.setVisible(true);
     }
 }
